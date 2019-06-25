eeb5d1444750e24ecc38e3ec1c95a2e787359288
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-25 19:58:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   squares = []
   i = 0 
   while array[i] do
-    new_square = array[i]**2 
-    squares.push(new_square)
+    array[i]**2 
+    .push(new_square)
   end 
   return squares
 end
